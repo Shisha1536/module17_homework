38c678387b193810865c29018d7f362bf2905dd2
Shisha1536 <135944742+Shisha1536@users.noreply.github.com>
2023-10-21 12:02:13 +0300

    finish

--- test/Index.js
+++ test/Index.js
@@ -1,4 +1,20 @@
-function getPercent(percent, number) {
+export function getPercent(percent, number) {
     return percent / number * 100;
 }
-module.exports = getPercent;
+export function getMonth(n) {
+	if (n === 1) return 'январь';
+	if (n === 2) return 'февраль';
+	if (n === 3) return 'март';
+	if (n === 4) return 'апрель';
+	if (n === 5) return 'май';
+	if (n === 6) return 'июнь';
+	if (n === 7) return 'июль';
+	if (n === 8) return 'август';
+	if (n === 9) return 'сентябрь';
+	if (n === 10) return 'октябрь';
+	if (n === 11) return 'ноябрь';
+	if (n === 12) return 'декабрь';
+}
+export function getNumber (n) {
+	return Number(n);
+}
--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,19 @@
-const getPercent = require('./Index');
+import { getPercent } from './Index'
+import { getMonth } from './Index'
+import { getNumber } from './Index'
 
-test('percentage of the number', () => {
-  expect(getPercent(25, 100)).toBe(30);
+describe('percentage of the number', () => {
+  it('true', () => expect(getPercent(25, 100)).toBe(25));
+  it('false', () => expect(getPercent(25, 100)).toBe(30));
+  it('calculation 0%', () => expect(getPercent(0, 100)).toBe(25));
+});
+describe('withdrawal of months', () => {
+  it('withdrawal of months январь', () => expect(getMonth(1)).toBe('январь'));
+  it('withdrawal of months февраль', () => expect(getMonth(2)).toBe('январь'));
+  it('withdrawal of months no', () => expect(getMonth(0)).toBe('январь'));
+});
+describe('Get a number', () => {
+  it('Get a number 123', () => expect(getNumber("123")).toBe(123));
+  it('Get a number 12', () => expect(getNumber('21')).toBe(12));
+  it('Get a number no', () => expect(getNumber('undefined')).toBe(123));
 });
